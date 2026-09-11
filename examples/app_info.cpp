8c8a58ee740c627c2400a1fc461224b31989c3ad
Lawrence <68999458+Lawrence-Link@users.noreply.github.com>
2026-09-11 11:20:20 +0800

    Prepare beta 0.5 release

--- examples/app_info.cpp
+++ examples/app_info.cpp
@@ -108,7 +108,7 @@ public:
         display.drawHLine(42, 37, 80);
 
         display.setFont(PIXELUI_FONT_SMALL);
-        display.drawStr(43, 50, "VERSION beta 0.4");
+        display.drawStr(43, 50, "VERSION beta 0.5");
 
         display.setFont(PIXELUI_FONT_TINY);
         display.drawStr(43, 61, "BSD-2  /  L. LINK");
